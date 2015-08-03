3df2131d11238a22dc8c46a542f090e404da79b3
durso <durso.rodrigo@gmail.com>
2015-08-03 19:06:26 +0100

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,11 +5,7 @@ use app\router;
 
 class bootstrap{
 
-    public function start(){
 
-        router::addRoute("/medico/id:int", array("controller" => "medico","action" =>"index"));
-
-    }
 }
 
  
